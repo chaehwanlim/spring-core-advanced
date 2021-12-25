64dde8cf8bff49e346669a72f1157c601ff9415a
chaehwanlim <chlim428@gmail.com>
2021-12-25 23:52:18 +0900

    동시성 문제가 발생하는 코드를 작성해 문제를 파악한다

---
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,48 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000);    // 동시성 문제가 발생하지 않게 하기 위해 2000millis
+        sleep(100); // 동시성 문제가 발생하는 코드
+        threadB.start();
+        sleep(3000);    // B 스레드가 끝날 때 까지 기다리기 위해
+
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
