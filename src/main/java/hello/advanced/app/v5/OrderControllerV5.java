deea3884b48d1da2d62b05306c8191a641a3e1cc
chaehwanlim <chlim428@gmail.com>
2022-01-02 22:09:57 +0900

    feat: LogTraceV5를 애플리케이션에 적용한다

---
+++ src/main/java/hello/advanced/app/v5/OrderControllerV5.java
@@ -0,0 +1,35 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.callback.TraceCallback;
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderControllerV5 {
+
+    private final OrderServiceV5 orderService;
+    private final LogTrace tracer;
+    private final TraceTemplate traceTemplate;
+
+    public OrderControllerV5(OrderServiceV5 orderService, LogTrace tracer) {
+        this.orderService = orderService;
+        this.tracer = tracer;
+        this.traceTemplate = new TraceTemplate(tracer);
+    }
+
+    @GetMapping("/v5/request")
+    public String request(String itemId) {
+        return traceTemplate.execute("OrderController.request()", new TraceCallback<>() {
+            @Override
+            public String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        });
+    }
+
+}
